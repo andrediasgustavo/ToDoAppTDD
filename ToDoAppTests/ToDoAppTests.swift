e8fcd5f156bc3025881c630433705b35dcb35068
Andre Dias <andrediasgustavo@gmail.com>
2017-06-20 00:09:18 -0300

    Add the inital project

---
+++ ToDoAppTests/ToDoAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  ToDoAppTests.swift
+//  ToDoAppTests
+//
+//  Created by Andre Dias on 19/06/17.
+//  Copyright © 2017 Andre Dias. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoApp
+
+class ToDoAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+}
